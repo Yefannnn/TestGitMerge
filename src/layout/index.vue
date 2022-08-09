2025bfc21b93bdd61951ade4e32f5774d1fc418a
小白 <65057505+Y1748373042@users.noreply.github.com>
2022-08-09 14:52:58 +0800

    tester01 首页底部添加h1标题测试

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -3,12 +3,16 @@
     <div class="top">
       <ul v-for="(item, ind) in title" :key="ind" class="headerUl">
         <router-link class="link" :to="item.meta.path">{{
-          item.meta.path
+            item.meta.path
         }}</router-link>
       </ul>
     </div>
     <div class="buttom">
       <router-view></router-view>
+    </div>
+
+    <div>
+      <h1> 我是测试字段</h1>
     </div>
   </div>
 </template>
@@ -31,10 +35,12 @@ export default {
   height: 60px;
   background-color: rgb(207, 248, 207);
 }
+
 .buttom {
   height: calc(100vh - 78px);
   background-color: rgb(199, 235, 255);
 }
+
 .headerUl {
   height: 100%;
   padding: 0;
@@ -44,6 +50,7 @@ export default {
   align-items: center;
   float: left;
 }
+
 .link {
   text-decoration: none;
   height: 35px;
